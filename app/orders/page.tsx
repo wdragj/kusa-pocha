afa85e3b6d7cdb62525e4ac2c36de0ab55fb690d
Freddy Seo <wdragj@gmail.com>
2024-06-09 19:22:49 -0500

    minor fix

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -1,15 +1,9 @@
 import { subtitle } from "@/components/primitives";
-import { auth } from "@/lib/auth";
 import ProfitAnalytics from "@/components/dashboard/profitAnalytics";
 import OrderAnalytics from "@/components/dashboard/orderAnalytics";
 import Orders from "@/components/dashboard/orders";
 
 export default async function OrdersPage() {
-  const session = await auth();
-  const user = session?.user;
-
-  console.log(session);
-
   return (
     <div>
       <ProfitAnalytics />
